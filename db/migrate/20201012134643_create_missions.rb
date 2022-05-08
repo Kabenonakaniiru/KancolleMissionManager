f6d0d3ccb96ac25c7c27ee214110d6e8c3690000
kabenonakaniiru <uhotayori@gmail.com>
2022-05-08 17:32:17 +0900

    bigintを付けるように修正

--- db/migrate/20201012134643_create_missions.rb
+++ db/migrate/20201012134643_create_missions.rb
@@ -2,7 +2,7 @@ class CreateMissions < ActiveRecord::Migration[6.0]
   def change
     create_table :missions do |t|
       t.string :name, null: false
-      t.references :classification, null: false, foreign_key: true
+      t.references :classification, null: false, type: :bigint, foreign_key: true
       t.boolean :limited, null: false, default: false
       t.string :url
       t.string :note
